573594fab3c0e1f50ce35f9b3221ced78095d249
Abdullahalbakhit <70815008+Abdullahalbakhit@users.noreply.github.com>
2020-09-09 20:45:08 +0300

    done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+let name = "عبدالله"
+var hobby1 = "gaming"
+var hobby2 = "وكرة القدم"
+var age = 15
+var height = 1.74
+print("اسمي \(name) انا احب البرمجه \(hobby2) عمري \(age) وطولي \(height)")
+
+let friendName = "ali"
+var friendHight = 1.75
+var friendAge = 27
+var friendHobby1 = "gaming"
+var friendhobby2 = "reading"
+var ageDifference = friendAge - (age)
+
+(name == friendName || age == friendAge)
+
+var multualHubby = (hobby1 == friendHobby1) || (hobby1 == friendhobby2) || (hobby2 == friendHobby1) || (hobby2 == friendhobby2)
+
